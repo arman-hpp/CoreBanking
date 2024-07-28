cdaa43a7f6a82062e11c984d2c9a36984c11eea9
arman-hpp <arman.hassanpoor2000@gmail.com>
2024-07-28 21:49:35 +0330

    add cache to CustomerService

--- src/main/java/com/bank/services/customers/CustomerService.java
+++ src/main/java/com/bank/services/customers/CustomerService.java
@@ -5,6 +5,12 @@ import com.bank.exceptions.DomainException;
 import com.bank.models.customers.Customer;
 import com.bank.repos.customers.CustomerRepository;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
@@ -16,12 +22,17 @@ public class CustomerService {
     private final CustomerRepository _customerRepository;
     private final ModelMapper _modelMapper;
 
-    private CustomerService(CustomerRepository customerRepository, ModelMapper modelMapper) {
+    private static final Logger log = LoggerFactory.getLogger(CustomerService.class);
+
+    public CustomerService(CustomerRepository customerRepository, ModelMapper modelMapper) {
         _customerRepository = customerRepository;
         _modelMapper = modelMapper;
     }
 
+    @Cacheable("customers")
     public List<CustomerDto> loadCustomers() {
+        log.info("loadCustomers called");
+
         var customers = _customerRepository.findAllByOrderByIdDesc();
         var customerDtoList = new ArrayList<CustomerDto>();
         for (var customer : customers) {
@@ -31,6 +42,7 @@ public class CustomerService {
         return customerDtoList;
     }
 
+    @Cacheable(value = "customer", key = "#customerId")
     public CustomerDto loadCustomer(Long customerId) {
         var customer = _customerRepository.findById(customerId).orElse(null);
         if (customer == null)
@@ -39,28 +51,42 @@ public class CustomerService {
         return _modelMapper.map(customer, CustomerDto.class);
     }
 
-    public void addCustomer(CustomerDto customerDto) {
-        var customer = _modelMapper.map(customerDto, Customer.class);
-        _customerRepository.save(customer);
+    @Caching(evict = {
+            @CacheEvict(value = "customers", allEntries = true)
+    }, put = {
+            @CachePut(cacheNames = "customer", key = "#customerDto.id")
+    })
+    public CustomerDto addOrEditCustomer(CustomerDto customerDto) {
+        if (customerDto.getId() == null || customerDto.getId() <= 0) {
+            return addCustomer(customerDto);
+        } else {
+            return editCustomer(customerDto);
+        }
     }
 
-    public void editCustomer(CustomerDto customerDto) {
+    private CustomerDto addCustomer(CustomerDto customerDto) {
+        var customer = _modelMapper.map(customerDto, Customer.class);
+        _customerRepository.save(customer);
+
+        customerDto.setId(customer.getId());
+        return customerDto;
+    }
+
+    private CustomerDto editCustomer(CustomerDto customerDto) {
         var customer = _customerRepository.findById(customerDto.getId()).orElse(null);
         if (customer == null)
             throw new DomainException("error.customer.notFound");
 
         _modelMapper.map(customerDto, customer);
         _customerRepository.save(customer);
+
+        return customerDto;
     }
 
-    public void addOrEditCustomer(CustomerDto customerDto) {
-        if (customerDto.getId() == null || customerDto.getId() <= 0) {
-            addCustomer(customerDto);
-        } else {
-            editCustomer(customerDto);
-        }
-    }
-
+    @Caching(evict = {
+            @CacheEvict(value = "customers", allEntries = true),
+            @CacheEvict(cacheNames = "customer", key = "#customerId")
+    })
     public void removeCustomer(Long customerId) {
         var customer = _customerRepository.findById(customerId).orElse(null);
         if (customer == null)
